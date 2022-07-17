afc0c1470bfec1cc44c720d1f6ac0e1728fb7b58
Taige Wang <taige.wang@aalto.fi>
2022-07-17 19:36:38 +0300

    Check e2e pipeline is working

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) { // eslint-disable-line no-unused-vars
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   video: false
-})
+});
