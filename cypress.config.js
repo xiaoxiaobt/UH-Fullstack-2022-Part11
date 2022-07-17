e50e36bae5bedfa404b07b21ae6f39a106cbc58a
Taige Wang <taige.wang@aalto.fi>
2022-07-17 21:30:24 +0300

    Trying to figure out why it doesn't work on GitHub

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
@@ -6,5 +6,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-  video: false
-})
+});
