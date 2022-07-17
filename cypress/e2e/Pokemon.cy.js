f109a9897bcc53bdd8bc5af2cabe3694214197f9
Taige Wang <taige.wang@aalto.fi>
2022-07-17 19:36:38 +0300

    Check e2e pipeline is working

--- cypress/e2e/Pokemon.cy.js
+++ cypress/e2e/Pokemon.cy.js
@@ -1,13 +1,7 @@
-describe('Pokedex', () => {
-  it('front page can be opened', () => {
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
     cy.visit('http://localhost:5000', { timeout: 120000 })
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
-
-  it('pokemon details can be found', () => {
-    cy.visit('http://localhost:5000', { timeout: 120000 })
-    cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
   })
 })
