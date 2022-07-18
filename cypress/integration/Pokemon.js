7b3fbfdb29fd2ffb672d3808219c35548e8a51cd
Taige Wang <xiaoxiaobt@gmail.com>
2022-07-18 09:04:01 +0300

    Update and rename Pokemon.cy.js to Pokemon.js

--- cypress/integration/Pokemon.js
+++ cypress/integration/Pokemon.js
@@ -1,5 +1,5 @@
 describe('Pokedex', () => {
-  it('front page can be opened', function () {
+  it('front page can be opened', () => {
     cy.visit('http://localhost:5000', { timeout: 120000 })
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
