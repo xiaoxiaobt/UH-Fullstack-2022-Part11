784ef09b519a9804f8acb8eb6816e7f78037da2a
Taige Wang <taige.wang@nokia.com>
2022-07-25 20:39:03 +0300

    Break app intentionally deliberately to test health status

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('false')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
